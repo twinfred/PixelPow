116173ab1185d0c80fcca69b64fd29d3e8797008
Timothy Winfred <twinfred@gmail.com>
2018-08-09 16:20:56 -0700

    completed About Us section of index.ejs

--- static/script/main.js
+++ static/script/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+// POPOUT MENU
     // Show Popout Menu
     $('#menu-icon-img').click(function(){
         $('#menu-popout').show("slide", {direction: "right"}, function(){
@@ -7,6 +9,7 @@ $(document).ready(function(){
             });
         });
     })
+
     // Hide Popout Menu
     function hidePopoutMenu(){
         $('#menu-x').fadeOut('fast', function(){
@@ -21,4 +24,48 @@ $(document).ready(function(){
     $('#menu-x').click(function(){
         hidePopoutMenu();
     })
+
+// HERO IMAGE SLIDER
+    const hero_images = ['/img/heroslider1.png','/img/heroslider2.png','/img/heroslider3.png']
+    var curr_img = 0;
+    var img_cnt = hero_images.length;
+
+    // Auto Rotate
+    setInterval(function(){
+        changeHeroImg(curr_img + 1);
+    }, 6000)
+
+    // Left Arrow Click
+    $('#hero-left-arrow').click(function(){
+        changeHeroImg(curr_img - 1);
+    })
+
+    // Right Arrow Click
+    $('#hero-right-arrow').click(function(){
+        changeHeroImg(curr_img + 1);
+    })
+
+    $('.hero-dot').click(function(){
+        let dot = $(this).attr('id');
+        dot = parseInt(dot[dot.length-1]);
+        changeHeroImg(dot);
+    })
+
+    function changeHeroImg(go_to_img){
+        if(go_to_img == curr_img){
+            return;
+        }
+        if(go_to_img >= img_cnt){
+            go_to_img = 0;
+        }
+        if(go_to_img < 0){
+            go_to_img = img_cnt - 1;
+        }
+        $('#hero-slider-img').fadeTo('slow', 0.1, function(){
+            $(this).css('background-image', 'url('+hero_images[go_to_img]+')');
+            $('#hero-dot-'+curr_img).css('background-image', 'url("/img/dot_black.png")');
+            $('#hero-dot-'+go_to_img).css('background-image', 'url("/img/dot_yellow.png")');
+            curr_img = go_to_img;
+        }).fadeTo('slow', 1);
+    }
 })
